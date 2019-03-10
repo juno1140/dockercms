6a6f3bdd281c11537100a46ccd260a13571f074f
kinoshita1140 <1bey1140@gmail.com>
2019-03-10 23:35:29 +0900

    Hello World!!

---
+++ metacms/resources/views/common/header.blade.php
@@ -0,0 +1,18 @@
+@section('header')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Laravelでサイト作り</title>
+    @yield('addCss')
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js">
+    <![endif]-->
+</head>
+<body>
+@stop
